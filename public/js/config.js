d8d29fdbeef4b43f3229d06d4d4f356884ab4baa
Venus <venus@venusworld.cn>
2018-04-01 16:13:35 +0800

    :bug:  fixed summernote bugs

--- public/js/config.js
+++ public/js/config.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
 		placeholder: '请输入你的评论内容',
 		lang: 'zh-CN',
 		height: 200,
-		maxHeight: 400
+		maxHeight: 400,
+		toolbar: [
+			// [groupName, [list of button]]
+			['style', ['bold', 'italic', 'underline', 'clear']],
+			['font', ['strikethrough', 'superscript', 'subscript']],
+			['fontsize', ['fontsize']],
+			['color', ['color']],
+			['para', ['ul', 'ol', 'paragraph']]
+		]
 	});
 });
