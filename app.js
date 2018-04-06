6be100bd5d71a44916c678d05e868b1eab0d3f7b
Venus <venus@venusworld.cn>
2018-04-06 12:41:08 +0800

    :sparkles: catch 404

--- app.js
+++ app.js
@@ -64,20 +64,14 @@ app.use('/category', category);
 app.use('/menu', menu);
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// catch 404
+app.use(function(req, res) {
 	var err = new Error('Not Found');
 	err.status = 404;
-	next(err);
-});
 
-// error handler
-app.use(function(err, req, res) {
-	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = err;
 
-	// render the error page
 	res.status(err.status || 500);
 	res.render('error');
 });
