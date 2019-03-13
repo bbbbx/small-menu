d5e784ffa2eae7a6d3e2d254b8245d91a5aea870
venus <venus@venusworld.cn>
2019-03-13 13:17:26 +0800

    rectify Promise usage

--- routes/login.js
+++ routes/login.js
@@ -16,7 +16,7 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
 	const { account, password, captcha } = req.body;
-	if (captcha !== req.session.captcha) {
+	if (captcha.toLowerCase() !== req.session.captcha.toLowerCase()) {
 		req.flash('error', '验证码错误！');
 		res.redirect('back');
 	} else {
@@ -28,56 +28,55 @@ router.post('/', function(req, res) {
 				Menu,
 				Comment
 			]
-		})
-			.then(user => {
-				if (!user) {
-					req.flash('error', '用户不存在!');
-					res.redirect('back');
-				} else if (user.confirmed) {
-					bcrypt.compare(password, user.password, (err, isMatch) => {
-						if (err) {
-							req.flash('error', err);
-							res.redirect('back');
-						} else if (isMatch) {
-							req.session.user = user.dataValues;
-							req.session.user.following = [];
-							req.session.user.followers = [];
-							req.session.user.collections = user.dataValues.menus;
-							req.session.user.collectedArticles = user.dataValues.articles;
-							req.session.user.articles = [];
-							req.session.user.articleComments = user.dataValues.articleComments;
-							req.session.user.comments = user.dataValues.comments;
+		}).then(user => {
+			if (!user) {
+				req.flash('error', '用户不存在!');
+				res.redirect('back');
+			} else if (user.confirmed) {
+				bcrypt.compare(password, user.password, (err, isMatch) => {
+					if (err) {
+						req.flash('error', err);
+						res.redirect('back');
+					} else if (isMatch) {
+						req.session.user = user.dataValues;
+						req.session.user.following = [];
+						req.session.user.followers = [];
+						req.session.user.collections = user.dataValues.menus;
+						req.session.user.collectedArticles = user.dataValues.articles;
+						req.session.user.articles = [];
+						req.session.user.articleComments = user.dataValues.articleComments;
+						req.session.user.comments = user.dataValues.comments;
 
-							user.getFollowing().then(followings => {
+						let followings = user.getFollowing();
+						let followers = user.getFollowers();
+						let articles = Article.findAll({
+							where: { userId: req.session.user.id}
+						});
+						Promise.all([followings, followers, articles])
+							.then(([followings, followers, articles]) => {
 								followings.map(value => {
 									req.session.user.following.push(value.dataValues);
 								});
-								user.getFollowers().then(followers => {
-									followers.map(value => {
-										req.session.user.followers.push(value.dataValues);
-									});
-									Article.findAll({
-										where: { userId: req.session.user.id}
-									}).then(articles => {
-										articles.map(value => {
-											req.session.user.articles.push(value.dataValues);
-										});
-										res.redirect(res.app.locals.Referer);  // res.app.locals.Referer 为在 GET /user 时存储的 `req.get('Referer')`
-									});
+								followers.map(value => {
+									req.session.user.followers.push(value.dataValues);
 								});
+								articles.map(value => {
+									req.session.user.articles.push(value.dataValues);
+								});
+								res.redirect(res.app.locals.Referer);  // res.app.locals.Referer 为在 GET /user 时存储的 `req.get('Referer')`
 							});
-						} else {
-							req.flash('error', '密码错误！');	
-							req.session.account = account;
-							res.redirect('back');
-						}
-					});
-				} else {
-					req.flash('error', '请先验证注册邮箱！');	
-					req.session.account = account;
-					res.redirect('back');
-				}
-			});
+					} else {
+						req.flash('error', '密码错误！');	
+						req.session.account = account;
+						res.redirect('back');
+					}
+				});
+			} else {
+				req.flash('error', '请先验证注册邮箱！');	
+				req.session.account = account;
+				res.redirect('back');
+			}
+		});
 		
 	}
 });
